c92e24d1fe8e62743da51f09812f79f9b1b27e06
suzanaai <angelska327@gmail.com>
2026-01-14 14:28:21 -0600

    token

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -16,4 +16,68 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
+// GET /hoots
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const hoots = await Hoot.find({})
+      .populate("author")
+      .sort({ createdAt: "desc" });
+    res.status(200).json(hoots);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+// Get /hoots/:hootsId
+
+router.get("/:hootId", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId).populate("author");
+    res.status(200).json(hoot);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// PUT /hoots/:hootId
+router.put("/:hootId", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+
+    //Check Permission:
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+    //Update Permissions
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+    //Update hoot:
+    const updateHoot = await Hoot.findByIdAndUpdate(
+      req.params.hootId,
+      req.body,
+      { new: true }
+    );
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+  //delete
+
+  // controllers/hoots.js
+
+  router.delete("/:hootId", verifyToken, async (req, res) => {
+    try {
+      const hoot = await Hoot.findById(req.params.hootId);
+
+      if (!hoot.author.equals(req.user._id)) {
+        return res.status(403).send("You're not allowed to do that!");
+      }
+
+      const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+      res.status(200).json(deletedHoot);
+    } catch (err) {
+      res.status(500).json({ err: err.message });
+    }
+  });
+
 module.exports = router;
